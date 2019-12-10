f4be09ce0dea28ee58a6164221196674fa176601
DougSchuindt <douglas.schuindt@gmail.com>
2019-12-10 20:55:18 -0300

    change #01

--- kube_setup.sh
+++ kube_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-
 # Install Docker
 curl -sSL get.docker.com | sh && \
 sudo usermod $USER -aG docker
 sudo systemctl enable docker
 
 # Install kubeadm
+sudo apt install curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
 sudo apt update -q && \
