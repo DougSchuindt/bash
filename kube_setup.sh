4cf1057a42302b996c346f9b61b0f746d8628167
DougSchuindt <douglas.schuindt@gmail.com>
2020-01-22 20:53:50 -0300

    Update kube_setup.sh

--- kube_setup.sh
+++ kube_setup.sh
@@ -18,10 +18,10 @@ sudo usermod $USER -aG docker
 sudo systemctl enable docker
 
 # Install kubeadm
-sudo apt install curl
+sudo apt install -y curl apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
 sudo apt update -q && \
-sudo apt install -qy kubeadm
+sudo apt install -qy kubeadm kubelet kubectl
 
 exit 0
